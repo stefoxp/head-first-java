7a41f0164c68275e1567ca6f31c8c2dbb9fd8a27
StefoXP <stefano.pasquini.2010@gmail.com>
2023-05-02 16:44:04 +0200

    MyFirstApp

---
+++ chp_01/Party.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class Party {
+    // public static void main(String[] args) is missing
+    public void buildInvite() {
+        Frame f = new Frame();
+        Label l = new Label("Party at Tim's");
+        Button b = new Button("You bet");
+        Button c = new Button("Shoot me");
+        Panel p = new Panel();
+        p.add(l);
+    }
+}
